c940090feaeb2d5da4904f807f54f30d737f1a2f
Marc Wainwright <34754301+MarcWainwright@users.noreply.github.com>
2017-12-26 22:04:45 -0800

    Add files via upload

---
+++ FloatingScriptScene/Assets/Script/Float.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour {
+
+	public float amplitude;          //Set in Inspector Only Edit these two variables in the inspector.
+	public float speed;              //Set in Inspector 
+	public float tempVal;
+	public Vector3 CurrentPos;
+	public Vector3 tempPos;
+	void Start () 
+	{
+		tempVal = transform.position.y;
+
+
+		//Un-comment this amplitude and comment out the one below it for more random results, change the values to your liking if you do so.
+		//amplitude = Random.Range (0.1f, 0.3f); 
+		amplitude = 0.2f;
+
+
+		//Un-comment this speed and comment out the one below it for more random results, change the values to your liking if you do so.
+		//speed = Random.Range(0.5f, 3.00f);
+		speed = 2.00f;
+	}
+	void Update ()
+	{        
+		tempPos.y = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+		tempPos.x = transform.position.x;
+		transform.position = tempPos;
+
+	}
+}
